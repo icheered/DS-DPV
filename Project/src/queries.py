73eed097a7877b39c1ac5fa231baafda5293bf2f
ICheered <tjeerd992@gmail.com>
2023-04-12 10:40:09 +0200

    Add room occupancy

--- Project/src/queries.py
+++ Project/src/queries.py
@@ -68,3 +68,48 @@ def get_overtime(df):
 
 
     return result_df
+
+
+def get_room_occupancy(dataframe):
+    dataframe['time_start'] = pd.to_datetime(dataframe['time_start'])
+    dataframe['time_end'] = pd.to_datetime(dataframe['time_end'])
+
+    unique_data = dataframe.drop_duplicates(subset=['date', 'room', 'time_start', 'time_end'])
+
+
+    grouped_data = unique_data.groupby(['date', 'room'])['duration'].sum().reset_index()
+    return grouped_data
+
+def get_room_occupancy_info(dataframe):
+    # Convert date column to datetime
+    dataframe['date'] = pd.to_datetime(dataframe['date'])
+    
+    # Calculate total room usage
+    room_usage = dataframe.groupby('room')['duration'].sum().reset_index()
+
+    # Calculate the number of educational days per room per year
+    dataframe['year'] = dataframe['date'].dt.year
+    educational_days_per_year = 200
+    room_year_counts = dataframe.groupby(['room', 'year'])['date'].nunique().reset_index()
+    room_year_counts.rename(columns={'date': 'days_count'}, inplace=True)
+    room_year_counts['available_time'] = room_year_counts['days_count'] * 480
+
+    # Calculate the maximum available time for each room
+    room_total_available_time = room_year_counts.groupby('room')['available_time'].sum().reset_index()
+
+    # Calculate occupancy percentage
+    room_info = pd.merge(room_usage, room_total_available_time, on='room')
+    room_info['occupancy_percentage'] = (room_info['duration'] / room_info['available_time'] * 100).round(1)
+
+    # Calculate room usage variance
+    room_usage_by_day = dataframe.groupby(['room', 'date'])['duration'].sum().reset_index()
+    room_usage_variance = (room_usage_by_day.groupby('room')['duration'].var().reset_index()).round()
+    room_usage_variance.rename(columns={'duration': 'variance'}, inplace=True)
+
+    # Merge the two dataframes
+    room_info = pd.merge(room_info, room_usage_variance, on='room')
+
+    # Filter rooms with occupancy >= 70%
+    high_occupancy_rooms = room_info[room_info['occupancy_percentage'] >= 70]
+
+    return high_occupancy_rooms, room_info
